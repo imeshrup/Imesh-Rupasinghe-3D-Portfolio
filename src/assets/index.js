afbe9c85d26b300adc140001696e835b35ef8974
Denuka <imeshrupasinghe50@gmail.com>
2024-06-11 14:42:24 +0530

    minor changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,11 +9,10 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
+import figma from './tech/figma.png';
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
@@ -42,11 +41,10 @@ export {
   close,
   css,
   docker,
-  figma,
   git,
   html,
+  figma,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
   redux,
